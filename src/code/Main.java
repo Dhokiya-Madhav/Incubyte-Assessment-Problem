134afc8a38260521bcf0c707ba1483a403810142
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-08-04 16:08:09 +0530

    [Refactor]: Comments added for class Main

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,10 +1,19 @@
 package code;
+
 public class Main {
     
     public static void main(String[] args) {
-        char[] commands = {'f','b','l','u'};
-        char dir='s';
-        Chandrayaan craft = new Chandrayaan(0,0,0,dir);
+        // Define an array of commands to control the spacecraft.
+        char[] commands = {'f', 'b', 'l', 'u'};
+
+        // Set the initial direction of the spacecraft ('s' for south).
+        char dir = 's';
+
+        // Create a new Chandrayaan object with the initial position at coordinates (0, 0, 0) and facing south ('s').
+        Chandrayaan craft = new Chandrayaan(0, 0, 0, dir);
+
+        // Control the spacecraft using the sequence of commands.
+        // The 'craftController' method will process the commands and update the spacecraft's position and direction accordingly.
         craft.craftController(commands);
     }
 }
