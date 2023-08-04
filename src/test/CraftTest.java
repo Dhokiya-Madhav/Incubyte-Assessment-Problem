2f52dbde02d2f85d4619fb4675e0e1efc221d047
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-08-04 14:20:22 +0530

    [Added]: Unit test for forward/backward on axis X

--- src/test/CraftTest.java
+++ src/test/CraftTest.java
@@ -8,17 +8,17 @@ public class CraftTest {
     private Chandrayaan craft;
 
     @Before
-    public void Layout(){
-        craft = new Chandrayaan(0,0,0,'n');
+    public void Layout() {
+        craft = new Chandrayaan(0, 0, 0, 'n');
     }
 
     @Test
-    public void isCraftInitialized(){
+    public void isCraftInitialized() {
         assertNotNull(craft);
     }
 
     @Test
-    public void testCraftForwardBackwardOnAxisY(){
+    public void testCraftForwardBackwardOnAxisY() {
         craft.movespacecraft('f');
         Assert.assertEquals(0, craft.x);
         Assert.assertEquals(1, craft.y);
@@ -31,5 +31,21 @@ public class CraftTest {
         Assert.assertEquals(0, craft.z);
         Assert.assertEquals('n', craft.direction);
     }
-    
+
+    @Test
+    public void testCraftForwardBackwardOnAxisX() {
+        craft.direction = 'e';
+        craft.movespacecraft('f');
+        Assert.assertEquals(1, craft.x);
+        Assert.assertEquals(0, craft.y);
+        Assert.assertEquals(0, craft.z);
+        Assert.assertEquals('e', craft.direction);
+
+        craft.movespacecraft('b');
+        Assert.assertEquals(0, craft.x);
+        Assert.assertEquals(0, craft.y);
+        Assert.assertEquals(0, craft.z);
+        Assert.assertEquals('e', craft.direction);
+    }
+
 }
