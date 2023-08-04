273e720bfa524013fb61a400f642477209589626
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-08-04 16:07:13 +0530

    [Refactor]: Comments added for class Chandrayaan

--- src/code/Chandrayaan.java
+++ src/code/Chandrayaan.java
@@ -1,11 +1,11 @@
 package code;
 
-import javax.swing.plaf.synth.SynthSpinnerUI;
-
 public class Chandrayaan {
-    public int x,y, z;
+    // Fields to store the spacecraft's current position and direction.
+    public int x, y, z;
     public char direction;
 
+    // Constructor to initialize the spacecraft's initial position and direction.
     public Chandrayaan(int x, int y, int z, char direction) {
         this.x = x;
         this.y = y;
@@ -13,118 +13,122 @@ public class Chandrayaan {
         this.direction = direction;
     }
 
+    // Method to move the spacecraft forward or backward in the current direction.
     public void moveSpaceCraft(char move) {
         switch (direction) {
             case 'u':
                 if (move == 'f')
-                    z++;
+                    z++; // Move forward in the up direction.
                 else if (move == 'b')
-                    z--;
+                    z--; // Move backward in the up direction.
                 break;
             case 'd':
                 if (move == 'f')
-                    z++;
+                    z++; // Move forward in the down direction.
                 else if (move == 'b')
-                    z--;
+                    z--; // Move backward in the down direction.
                 break;
             case 'n':
                 if (move == 'f')
-                    y++;
+                    y++; // Move forward in the north direction.
                 else if (move == 'b')
-                    y--;
+                    y--; // Move backward in the north direction.
                 break;
             case 's':
                 if (move == 'f')
-                    y++;
+                    y++; // Move forward in the south direction.
                 else if (move == 'b')
-                    y--;
+                    y--; // Move backward in the south direction.
                 break;
             case 'e':
                 if (move == 'f')
-                    x++;
+                    x++; // Move forward in the east direction.
                 else if (move == 'b')
-                    x--;
+                    x--; // Move backward in the east direction.
                 break;
             case 'w':
                 if (move == 'f')
-                    x++;
+                    x++; // Move forward in the west direction.
                 else if (move == 'b')
-                    x--;
+                    x--; // Move backward in the west direction.
                 break;
         }
     }
 
+    // Method to turn the spacecraft left based on the current direction.
     public char turnLeft(char currentDirection) {
         switch (currentDirection) {
             case 'n':
-                direction = 'w';
+                direction = 'w'; // Turn left from north, face west.
                 break;
             case 'w':
-                direction = 's';
+                direction = 's'; // Turn left from west, face south.
                 break;
             case 's':
-                direction = 'e';
+                direction = 'e'; // Turn left from south, face east.
                 break;
             case 'e':
-                direction = 'n';
+                direction = 'n'; // Turn left from east, face north.
                 break;
             case 'u':
             case 'd':
-                direction = 'w';
+                direction = 'w'; // Turn left from up or down, face west.
                 break;
         }
         return direction;
     }
 
+    // Method to turn the spacecraft right based on the current direction.
     public char turnRight(char currentDirection) {
         switch (currentDirection) {
             case 'n':
-                direction = 'e';
+                direction = 'e'; // Turn right from north, face east.
                 break;
             case 'e':
-                direction = 's';
+                direction = 's'; // Turn right from east, face south.
                 break;
             case 's':
-                direction = 'w';
+                direction = 'w'; // Turn right from south, face west.
                 break;
             case 'w':
-                direction = 'n';
+                direction = 'n'; // Turn right from west, face north.
                 break;
             case 'u':
             case 'd':
-                direction = 'e';
+                direction = 'e'; // Turn right from up or down, face east.
                 break;
         }
         return direction;
     }
 
+    // Method to control the spacecraft using a sequence of commands.
     public void craftController(char[] commands) {
         for (int i = 0; i < commands.length; i++) {
             char command = commands[i];
             switch (command) {
                 case 'l':
-                    direction = turnLeft(direction);
+                    direction = turnLeft(direction); // Turn left.
                     break;
                 case 'r':
-                    direction = turnRight(direction);
+                    direction = turnRight(direction); // Turn right.
                     break;
                 case 'u':
-                    direction = 'u';
-                    break;
                 case 'd':
-                    direction = 'd';
+                    direction = command; // Move up or down.
                     break;
                 case 'f':
                 case 'b':
-                    moveSpaceCraft(command);
+                    moveSpaceCraft(command); // Move forward or backward.
                     break;
                 default:
                     System.out.println("Invalid Command");
             }
-            displayPosition();
         }
+        // Display the final position and direction of the spacecraft after executing the commands.
+        displayPosition();
     }
 
+    // Method to display the final position and direction of the spacecraft.
     public void displayPosition() {
         System.out.println("Final position: ");
         System.out.print(x + " " + y + " " + z + "\t");
