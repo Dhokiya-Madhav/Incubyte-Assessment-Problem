c09280d594eaf9ef3a78b9297e85d07a852af773
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-08-04 15:38:35 +0530

    [Refactor]: Class Main

--- src/code/Main.java
+++ src/code/Main.java
@@ -5,32 +5,6 @@ public class Main {
         char[] commands = {'f','b','l','u'};
         char dir='s';
         Chandrayaan craft = new Chandrayaan(0,0,0,dir);
-        for (int i = 0; i < commands.length; i++) {
-            char move = commands[i];
-            switch (move) {
-                case 'l':
-                    dir = craft.takeLeft(dir);
-                    break;
-                case 'r':
-                    dir = craft.takeRight(dir);
-                    break;
-                case 'u':
-                    dir = 'u';
-                    craft.direction='u';
-                    break;
-                case 'd':
-                    dir = 'd';
-                    craft.direction='d';
-                    break;
-                case 'f':
-                case 'b':
-                    craft.movespacecraft(move);
-                    break;
-                default:
-                    System.out.println("Invalid Command");
-                    
-            }
-            craft.displayPosition();
-        }
+        craft.craftController(commands);
     }
 }
